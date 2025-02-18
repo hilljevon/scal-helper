2c56f97b55e7e0783b95d86d1fdb658f9e8919ac
Jevon Hill <jevon623@gmail.com>
2025-02-18 12:44:53 -0800

    Corrected verbiage + formatting/

--- components/user/ClinReqPDF.tsx
+++ components/user/ClinReqPDF.tsx
@@ -6,6 +6,7 @@ import { FacilityType } from '@/lib/data';
 const styles = StyleSheet.create({
     page: { padding: 30, fontSize: 12, fontFamily: "Helvetica" },
     header: { display: "flex", justifyContent: 'center', alignItems: "center", width: "100%" },
+    dateHeader: { marginBottom: 10 },
     section: { marginBottom: 10 },
     title: { fontSize: 16, fontWeight: "bold", marginBottom: 10 },
     text: { marginBottom: 5 },
@@ -17,6 +18,8 @@ const styles = StyleSheet.create({
     tableRow: { flexDirection: "row" },
     tableCellHeader: { flex: 1, borderWidth: 1, padding: 5, fontWeight: "bold", backgroundColor: "#f2f2f2", textAlign: "center" },
     tableCell: { flex: 1, borderWidth: 1, padding: 5, textAlign: "center" },
+    caseManagementText: { fontWeight: "bold", textDecoration: "underline", fontSize: 14 },
+    mainContainer: { marginBottom: 15 }
 });
 interface ClinReqPatientsInterface {
     label: string,
@@ -27,35 +30,7 @@ interface ClinReqPatientsInterface {
     nkf: string,
     admitDate: string
 }
-// const clinReqPatients: ClinReqPatientsInterface[] = [
-//     {
-//         "label": "ERIC CHILDRESS: 6/3/1949",
-//         "value": "ERIC CHILDRESS: 6/3/1949",
-//         "name": "ERIC CHILDRESS",
-//         "dob": "6/3/1949",
-//         "mrn": "00-233167",
-//         "nkf": "WESTERN ARIZONA REGIONAL MEDICAL CENTER",
-//         "admitDate": "2/3/2025 12:00 PM"
-//     },
-//     {
-//         "label": "DONNA CHILDRESS: 2/17/1949",
-//         "value": "DONNA CHILDRESS: 2/17/1949",
-//         "name": "DONNA CHILDRESS",
-//         "dob": "2/17/1949",
-//         "mrn": "00-233169",
-//         "nkf": "Desert Regional Medical Center",
-//         "admitDate": "1/8/2025 2:43 PM"
-//     },
-//     {
-//         "label": "ROLAND PALMER: 6/3/1949",
-//         "value": "ROLAND PALMER: 6/3/1949",
-//         "name": "ROLAND PALMER",
-//         "dob": "6/3/1949",
-//         "mrn": "00-233171",
-//         "nkf": "UC IRVINE MEDICAL CENTER",
-//         "admitDate": "2/2/2025 4:27 PM"
-//     }
-// ]
+
 // Create Document Component
 const ClinReqPDF = ({ date, facility, clinReqPatients }: { date: string, facility: FacilityType, clinReqPatients: ClinReqPatientsInterface[] }) => (
     <Document>
@@ -64,43 +39,47 @@ const ClinReqPDF = ({ date, facility, clinReqPatients }: { date: string, facilit
                 <Image style={styles.image} src={"/images/ClinReqImage.png"} />
             </View>
             <View style={styles.section}>
-                <Text style={styles.title}>Clinical Request Form</Text>
-                <Text style={styles.text}>
+                <View style={styles.section}>
                     <Text style={styles.bold}>Date: {date} </Text>
-                </Text>
+                </View>
                 <Text style={styles.text}>
                     <Text style={styles.bold}>Hospital Name: {facility.name} </Text>
                 </Text>
                 <Text style={styles.text}>
-                    <Text style={styles.bold}>Fax Number:</Text> {facility.urReviewFax}
+                    <Text style={styles.bold}>Fax Number: {facility.urReviewFax}</Text>
                 </Text>
             </View>
 
             <View style={styles.section}>
-                <Text>
-                    Attention: <Text style={styles.bold}>Case Management / Utilization Management</Text>
-                </Text>
-                <Text style={styles.lineBreak}>
-                    We understand that our member may be receiving care at your facility. Please fax initial and/or concurrent
-                    clinical information to (562) 658-4101.
-                </Text>
-                <Text>
+                <View style={styles.section}>
+                    <Text style={styles.caseManagementText}>
+                        Attention: Case Management / Utilization Management
+                    </Text>
+                </View>
+                <View style={styles.section}>
+                    <Text style={styles.lineBreak}>
+                        We understand that our member may be receiving care at your facility. Please fax initial and/or concurrent
+                        clinical information to (562) 658-4101.
+                    </Text>
+                </View>
+
+                <Text style={styles.mainContainer}>
                     A Kaiser Permanente physician is always available on request if your treating physician wishes to take
                     advantage of the opportunity to address our member's current care needs, to provide access to our member's
-                    clinical history, and to discuss our member's care to ensure mutual understanding and agreement.
-                </Text>
-                <Text>
-                    Your physician may also choose to discuss our member's care and prior medical history with one of our case
+                    clinical history, and to discuss our member's care to ensure mutual understanding and agreement. Your physician may also choose to discuss our member's care and prior medical history with one of our case
                     managers.
                 </Text>
             </View>
 
             <View style={styles.section}>
                 <Text>Thank you for your cooperation,</Text>
+
+            </View>
+            <View>
                 <Text>Outside Utilization Resource Services (OURS)</Text>
+                <Text>Department 562-658-4100</Text>
                 <Text>Kaiser Foundation Health Plan, Southern California Division</Text>
             </View>
-
             {/* Table for Patient Info */}
             <View style={styles.table}>
                 <View style={styles.tableRow}>
